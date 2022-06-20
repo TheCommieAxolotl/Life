c6aa3a78414ec06557babf61d358a29d0edd7b8a
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-06-21 08:15:28 +1000

    [rollup] Terser, [util > life] Meta

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,33 @@
+import { terser } from "rollup-plugin-terser";
+
 export default [
     {
         input: "./src/index.mjs",
-        output: {
-            file: "./dist/cjs/index.cjs",
-            format: "cjs",
-            compact: true,
-        },
-    },
-    {
-        input: "./src/index.mjs",
-        output: {
-            file: "./dist/mjs/index.mjs",
-            format: "esm",
-            compact: true,
-        },
+        output: [
+            {
+                file: "./dist/cjs/index.cjs",
+                format: "cjs",
+                compact: true,
+                plugins: [
+                    terser({
+                        format: {
+                            comments: "all",
+                        },
+                    }),
+                ],
+            },
+            {
+                file: "./dist/mjs/index.mjs",
+                format: "esm",
+                compact: true,
+                plugins: [
+                    terser({
+                        format: {
+                            comments: "all",
+                        },
+                    }),
+                ],
+            },
+        ],
     },
 ];
--- src/util/life.mjs
+++ src/util/life.mjs
@@ -12,4 +12,24 @@ export const Life = {
     passTime: () => {
         Life.Animals.forEach((animal) => animal.ageUp());
     },
+    /**
+     * @name Meta
+     * @description Object containing metadata about lifejs
+     */
+    Meta: new (class {
+        get version() {
+            return "1.0.1";
+        }
+        plugins = [];
+
+        /**
+         * @name registerPlugin
+         * @description Registers a plugin with lifejs
+         * @param {LifePlugin} plugin - lifejs plugin exported Object
+         */
+        registerPlugin(plugin) {
+            this.plugins.push(plugin);
+            plugin.initilise();
+        }
+    })(),
 };
